ca178cc2f3f5095d291d25a0cc3c25cdbc94babe
Mauricio Soares <mauricio.massayuki@gmail.com>
2025-04-17 16:00:15 -0300

    feat: added colors to plotting data, and ui cards from shiny components

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,17 +8,30 @@ file_path = Path(__file__).parent / "penguins.csv"
 df = pd.read_csv(file_path)
 print(df)
 
+
+with ui.sidebar(bg="#f8f8f8"):
+    ui.input_slider("mass", "Max Body Mass", 2000, 8000, 6000)
+
+
 ui.h1("Dashboard")
 ui.p("Text")
-ui.input_slider("mass", "Max Body Mass", 2000, 8000, 6000)
 
 
-@render_plotly
-def plot():
-    df_subset = df[df["body_mass_g"] < input.mass()]
-    return px.scatter(df_subset, x="bill_depth_mm", y="bill_length_mm")
+with ui.layout_columns():
+    with ui.card():
+        @render_plotly
+        def plot():
+            df_subset = df[df["body_mass_g"] < input.mass()]
+            if input.show_species():
+                return px.scatter(
+                    df_subset, x="bill_depth_mm", y="bill_length_mm", color="species"
+                )
+            return px.scatter(df_subset, x="bill_depth_mm", y="bill_length_mm")
+        
+        ui.input_checkbox("show_species", "Show Species", value=True)
 
 
-@render.data_frame
-def data():
-    return df[df["body_mass_g"] < input.mass()]
+    with ui.card():
+        @render.data_frame
+        def data():
+            return df[df["body_mass_g"] < input.mass()]
