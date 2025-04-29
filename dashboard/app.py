0ecff65a80c120e8bcd3d0d0dab4d6710bab58e8
Mauricio Soares <mauricio.massayuki@gmail.com>
2025-04-29 15:40:20 -0300

    feat: added reactive.calc to prevent the application from re-rendering data multiple times

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,10 +2,22 @@ from shiny.express import ui, render, input
 from shinywidgets import render_plotly
 import plotly.express as px
 from data_import import df
+from shiny import reactive
 
 
 with ui.sidebar(bg="#f8f8f8"):
     ui.input_slider("mass", "Max Body Mass", 2000, 8000, 6000)
+    ui.input_checkbox_group(
+        "species",
+        "Species",
+        ["Adelie", "Chinstrap", "Gentoo"],
+        selected=["Adelie", "Chinstrap", "Gentoo"],
+    )
+
+
+@reactive.calc
+def filter_data():
+    return df[(df["species"].isin(input.species())) & df["body_mass_g"] < input.mass()]
 
 
 ui.h1("Dashboard")
@@ -14,19 +26,21 @@ ui.p("Text")
 
 with ui.layout_columns():
     with ui.card():
+
         @render_plotly
         def plot():
-            df_subset = df[df["body_mass_g"] < input.mass()]
+            df_subset = filter_data()
+
             if input.show_species():
                 return px.scatter(
                     df_subset, x="bill_depth_mm", y="bill_length_mm", color="species"
                 )
             return px.scatter(df_subset, x="bill_depth_mm", y="bill_length_mm")
-        
+
         ui.input_checkbox("show_species", "Show Species", value=True)
 
-
     with ui.card():
+
         @render.data_frame
         def data():
-            return df[df["body_mass_g"] < input.mass()]
+            return filter_data()
